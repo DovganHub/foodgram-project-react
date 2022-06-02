91c8b74e28845fdda7cbe828abe1f31e1068bccc
DovganHub <asdovganyuk@gmail.com>
2022-06-03 01:07:44 +0300

    fix445

--- backend/logic/permissions.py
+++ backend/logic/permissions.py
@@ -1,35 +1,19 @@
-# from rest_framework import permissions
+from rest_framework import permissions
 
 
-# class AuthorOrAdminOrRead(permissions.BasePermission):
-
-#     def has_object_permission(self, request, view, obj):
-#         if (request.method in ['PUT', 'PATCH', 'DELETE'] and not
-#                 request.user.is_anonymous):
-#             return (
-#                 request.user == obj.author
-#                 or request.user.is_superuser
-#             )
-#         return request.method in permissions.SAFE_METHODS
-
-#     def has_permission(self, request, view):
-#         return (
-#             request.method == 'POST' and
-#             request.user.is_authenticated
-#             or request.method in permissions.SAFE_METHODS)
-
-from rest_framework.permissions import (BasePermission,
-                                        IsAuthenticated)
-
-
-class IsOwnerProfile(IsAuthenticated):
+class AuthorOrAdminOrRead(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        if (request.method in ['PUT', 'PATCH', 'DELETE'] and not
+                request.user.is_anonymous):
+            return (
+                request.user == obj.author
+                or request.user.is_superuser
+            )
+        return request.method in permissions.SAFE_METHODS
 
-
-class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_staff
-        )
+        return (
+            request.method == 'POST' and
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS)
