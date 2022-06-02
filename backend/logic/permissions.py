ee6a918fb88f749b94d25eaa64d800ccbd5237ba
DovganHub <asdovganyuk@gmail.com>
2022-06-03 01:04:11 +0300

    fix444

--- backend/logic/permissions.py
+++ backend/logic/permissions.py
@@ -17,18 +17,19 @@
 #             request.method == 'POST' and
 #             request.user.is_authenticated
 #             or request.method in permissions.SAFE_METHODS)
-from rest_framework import permissions
+
+from rest_framework.permissions import (BasePermission,
+                                        IsAuthenticated)
 
 
-class AuthorOrAdminOrRead(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in ('POST', 'PATCH', 'DELETE') and
-            request.user.is_authenticated
-            or request.method in permissions.SAFE_METHODS)
+class IsOwnerProfile(IsAuthenticated):
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_superuser
-            or request.user == obj.author)
+        return obj.user == request.user
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_staff
+        )
