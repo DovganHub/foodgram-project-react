9d184c4e9925d5ff654cc8b90ec6b87010d6eb59
DovganHub <asdovganyuk@gmail.com>
2022-06-03 11:37:47 +0300

    reverted

--- backend/logic/permissions.py
+++ backend/logic/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class AdminOrAuthorOrReadOnly(permissions.BasePermission):
+class AdminOrAuthorOrRead(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.method == 'POST':
